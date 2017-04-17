d063ac65af5186e9df8d9dcdf89a2415a159de6b
Constantin Tsukanov <c@indotech.ru>
2017-04-17 09:30:18 +0300

    Fix zip-stream issue

--- template.js
+++ template.js
@@ -3,17 +3,21 @@ const unzip = require('unzip');
 const zip = require('zip-stream');
 
 function docxTemplate(props, docx) {
-  const archive = new zip({ zlib: { level: 1 } });
+  const archive = new zip({ zlib: { level: 0 } });
   docx.pipe(unzip.Parse()).
   on('entry', function (entry) {
     var fileName = entry.path;
     if (fileName === "word/document.xml") {
       const stream = entry.pipe(new Template(props));
-      archive.entry(stream, { name: fileName });
+      process.nextTick(() => archive.entry(stream, { name: fileName }));
     } else {
-      archive.entry(entry, { name: fileName });
       if (fileName === '[Content_Types].xml') {
-        archive.finalize();
+        process.nextTick(() => {
+          archive.entry(entry, { name: fileName })
+          archive.finalize();
+        });
+      } else {
+        process.nextTick(() => archive.entry(entry, { name: fileName }));
       }
     }
   });
