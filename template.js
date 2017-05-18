4c92349f755866b5432fc6c86804a62d9ec6d176
Constantin Tsukanov <c@indotech.ru>
2017-05-18 18:20:26 +0300

    Fix for some docx files

--- template.js
+++ template.js
@@ -4,24 +4,29 @@ const zip = require('zip-stream');
 
 function docxTemplate(props, docx) {
   const archive = new zip({ zlib: { level: 0 } });
-  docx.pipe(unzip.Parse()).
-  on('entry', function (entry) {
+  const rs = docx.pipe(unzip.Parse());
+  rs.on('entry', function (entry) {
     var fileName = entry.path;
     if (fileName === "word/document.xml") {
       const stream = entry.pipe(new Template(props));
       process.nextTick(() => archive.entry(stream, { name: fileName }));
     } else {
-      if (fileName === '[Content_Types].xml') {
-        process.nextTick(() => {
-          archive.entry(entry, { name: fileName })
-          archive.finalize();
-        });
-      } else {
-        process.nextTick(() => archive.entry(entry, { name: fileName }));
-      }
+      process.nextTick(() => archive.entry(entry, { name: fileName }));
     }
   });
+
+  rs.once('close', () => {
+    archive.finalize();
+  });
+
+  rs.once('error', () => {
+    archive.removeAllListeners();
+    if (archive.destroy) archive.destroy();
+    docx.destroy();
+  });
+
   archive.once('error', () => {
+    rs.removeAllListeners();
     docx.destroy();
   });
   return archive;
